d9d966d7ab298366d05beac7f2597da2aaf9b786
EBorgula <eborgula@umich.edu>
2025-07-14 18:29:18 -0400

    Delete button in progress

--- Assets/CollisionLogger.ts
+++ Assets/CollisionLogger.ts
@@ -1,5 +1,7 @@
 // CollisionLogger.ts
 import { InteractableManipulation } from "SpectaclesInteractionKit/Components/Interaction/InteractableManipulation/InteractableManipulation"
+import { Interactable } from "SpectaclesInteractionKit.lspkg/Components/Interaction/Interactable/Interactable";
+import { PinchButton } from "SpectaclesInteractionKit/Components/UI/PinchButton/PinchButton";
 
 
 
@@ -14,55 +16,68 @@ export class CollisionLogger extends BaseScriptComponent {
     
     @input
     public InManip: InteractableManipulation;
+
     
     public reset: Boolean;
 
     
     onAwake(): void {
         // Make collider intangible so it doesn’t block physics but can detect overlaps
-        this.collider.intangible = true;
+        if (this.collider) {
+            this.collider.intangible = true;
+        }
         this.reset = true;
-        this.InManip.showTranslationProperties = true;
+        if (this.InManip) {
+            this.InManip.showTranslationProperties = true;
+        }
         // Each time an overlap starts, print a message and reset the collider state
+        // this.createEvent("OnStartEvent").bind(() => {
+        //     if (this.pinchButton) {
+        //         this.pinchButton.onButtonPinched.add(() => {
+        //             if (this.widget) {
+        //                 this.widget.destroy();
+        //             }
+        //             print(`I pusha da buttin me dood`);
+        //         });
+        //     }
+        // });
 
-
+        if (this.collider) {
             this.collider.onOverlapEnter.add((eventArgs: any) => {
                 if (this.reset == true) {
-
                     const otherName: string = eventArgs.overlap.collider
                         .getSceneObject()
                         .name;
                     var result: Boolean
                     result = true;
                     print(`🔶 Overlapped with: ${otherName}`);
-                    
-                    if (otherName == 'Terrain'){
-                       this.InManip.setCanTranslate(false);
 
-                       this.collider.enabled = false;
+                    if (otherName == 'Terrain' && this.InManip) {
+                        this.InManip.setCanTranslate(false);
+                        this.collider.enabled = false;
                     }
 
-                    result = this.InManip.canTranslate();
-                    print(result);
+                    if (this.InManip) {
+                        result = this.InManip.canTranslate();
+                        print(result);
+                    }
 
                     // Reset the collider to allow the same object to trigger again
                     this.reset = false;
-                    //print(`The flag has been made: ${this.reset}`);
                 }
             });
-            
-             this.collider.onOverlapExit.add((eventArgs: any) => {
-                 if (this.reset == false) {
 
-                     const otherName: string = eventArgs.overlap.collider
-                         .getSceneObject()
-                         .name;
-                     print(`🔶 Stopped Overlap with: ${otherName}`);
+            this.collider.onOverlapExit.add((eventArgs: any) => {
+                if (this.reset == false) {
+                    const otherName: string = eventArgs.overlap.collider
+                        .getSceneObject()
+                        .name;
+                    print(`🔶 Stopped Overlap with: ${otherName}`);
 
-                     // Reset the collider to allow the same object to trigger again
-                     this.reset = true;
-                     //print(`The flag has been made: ${this.reset}`);
-                 }
-             });
+                    // Reset the collider to allow the same object to trigger again
+                    this.reset = true;
+                }
+            });
+        }
     }
 }
---
+++ Assets/DeleteButton.ts
@@ -0,0 +1,53 @@
+import { ToggleButton } from "SpectaclesInteractionKit/Components/UI/ToggleButton/ToggleButton";
+import { Interactable } from "SpectaclesInteractionKit/Components/Interaction/Interactable/Interactable";
+import { PinchButton } from "SpectaclesInteractionKit/Components/UI/PinchButton/PinchButton";
+import { InteractableOutlineFeedback } from "SpectaclesInteractionKit/Components/Helpers/InteractableOutlineFeedback";
+
+@component
+export class NewScript extends BaseScriptComponent {
+    
+    @input private deleteButton: PinchButton;
+    @input private noteInteractable: Interactable;
+    @input private noteMesh: RenderMeshVisual;
+    @input private menuObject : SceneObject;
+
+    private lastHoveredTime: number = -1;
+    private timeToShowButtonsAfterHover = 4;
+    private outlineFeedback: InteractableOutlineFeedback;
+    private editOutlineMaterial: Material;
+    private distanceOffset = 60;
+
+    onAwake() {
+        this.createEvent("OnStartEvent").bind(this.onStart.bind(this));
+        this.createEvent("UpdateEvent").bind(this.onUpdate.bind(this));
+
+        
+    }
+
+    private onStart() {
+        this.deleteButton.onButtonPinched.add(() => {
+      this.sceneObject.enabled = false;
+      this.sceneObject.destroy();
+      
+    });
+
+    this.noteInteractable.onHoverUpdate.add(() => {
+         this.lastHoveredTime = getTime();
+         });
+
+    }
+
+    private onUpdate() {
+    if (getTime() - this.timeToShowButtonsAfterHover < this.lastHoveredTime && this.menuObject.getTransform().getWorldPosition().distance(this.sceneObject.getTransform().getWorldPosition()) > this.distanceOffset) {
+      this.deleteButton.getSceneObject().enabled = true;
+    } 
+    else {
+        this.deleteButton.getSceneObject().enabled = false;
+    }
+
+    }
+
+
+
+
+}
